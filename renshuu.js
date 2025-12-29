15a7fcdc15efe7d1f8df05ead10d29b8c6b59d6f
buruburukun <buruburukun@proton.me>
2025-12-28 19:59:26 -0800

    link answer to dictionary

--- renshuu.js
+++ renshuu.js
@@ -13,7 +13,6 @@
     'use strict';
 
     function waitForElms(selector) {
-        console.log("waitForElms", selector);
         return new Promise(resolve => {
             const exist = document.querySelectorAll(selector);
             if (exist.length > 0) {
@@ -49,27 +48,57 @@
         };
     }
 
-    function linkToDictionary(selector) {
+    function forever(selector, action) {
         const func = () => {
             waitForElms(selector).then(elems => {
-                for (const elem of elems) {
-                    for (const node of elem.childNodes) {
-                        if (node.nodeType == Node.TEXT_NODE && node.nodeValue.trim().length > 0) {
-                            const link = document.createElement("span");
-                            link.textContent = node.textContent;
-                            link.setAttribute("class", "buru_dict_link");
-                            link.setAttribute("buru_dict", elem.textContent);
-                            link.addEventListener("click", delayEvent(vocabSearch, link));
-                            node.replaceWith(link);
-                        }
-                    }
-                }
+                elems.forEach(action);
                 func();
             });
         };
         func();
     }
 
+    function linkToDictionary(elem) {
+        for (const node of elem.childNodes) {
+            if (node.nodeType == Node.TEXT_NODE && node.nodeValue.trim().length > 0) {
+                const link = document.createElement("span");
+                link.textContent = node.textContent;
+                link.setAttribute("class", "buru_dict_link");
+                link.setAttribute("buru_dict", elem.textContent);
+                link.addEventListener("click", delayEvent(vocabSearch, link));
+                node.replaceWith(link);
+            }
+        }
+    }
+
     const selector = "div.print_term > div.flexbox > div.grow > div:not(.term_pitch_box) > span:not(:has(.buru_dict_link))";
-    linkToDictionary(selector);
+    forever(selector, linkToDictionary);
+
+    function linkAnswerToDictionary(elem) {
+        let kanji = "";
+        let kana = "";
+        const rubies = [];
+        for (const child of elem.children) {
+            if (child.tagName === "RUBY") {
+                if (child.childNodes[0].tagName === "SPAN") {
+                    kanji += child.childNodes[0].textContent;
+                    kana += child.childNodes[1].textContent;
+                } else {
+                    kanji += child.childNodes[0].textContent;
+                    kana += child.childNodes[0].textContent;
+                }
+            }
+        }
+        const query = `${kanji}/${kana}`;
+        const link = document.createElement("span");
+        link.textContent = query;
+        link.setAttribute("class", "buru_dict_link");
+        link.setAttribute("buru_dict", query);
+        link.addEventListener("click", delayEvent(vocabSearch, link));
+        const div = document.createElement("div");
+        div.append(link);
+        elem.prepend(div);
+    }
+    const answerSelector = "div.full_term > div > div > div > div.grow:not(:has(.buru_dict_link))";
+    forever(answerSelector, linkAnswerToDictionary);
 })();
